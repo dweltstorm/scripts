8c2a0c24469000ef9bf287af812739b1062537b7
dweltstorm <90920200+dweltstorm@users.noreply.github.com>
2023-03-29 01:39:48 -0700

    Create script.user.js

---
+++ reddit-unblock/script.user.js
@@ -0,0 +1,13 @@
+// ==UserScript==
+// @name         Reddit Unblock
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  Changes all reddit links to safereddit.com
+// @author       You
+// @match        https://*/*
+// @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
+// @grant        none
+// @require      https://code.jquery.com/jquery-3.6.4.min.js
+// ==/UserScript==
+
+Array.from(document.links).filter(x => /^https:\/\/www.reddit.com\//g.test(x.href)).forEach(x => {x.href = x.href.replace('www.reddit.com', 'safereddit.com')})
