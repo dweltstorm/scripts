c58c668e88778518763742ce81fffb103bfba276
dweltstorm <90920200+dweltstorm@users.noreply.github.com>
2024-02-10 15:09:29 -0800

    Create script.user.js

---
+++ kwan-serializer/script.user.js
@@ -0,0 +1,44 @@
+// ==UserScript==
+// @name        Kwan Serializer
+// @namespace   Violentmonkey Scripts
+// @match       https://lms.lausd.net/assignment/*/assessment_view/*
+// @match       https://lms.lausd.net/assignment/*/assessment
+// @grant       GM_setClipboard
+// @version     1.0
+// @author      dweltstorm
+// @description Lets you easily export and import answers for kwan homework
+// @require     https://cdnjs.cloudflare.com/ajax/libs/lz-string/1.5.0/lz-string.min.js
+// @require     https://code.jquery.com/jquery-3.7.1.js
+// ==/UserScript==
+/*globals $*/
+
+
+
+function getAnswers() {
+  return Object.values($(".text-blank-spot.selected")).slice(0, -2).map(el => (el.className.includes("incorrect")?"":el.innerText).replace("\nCorrect.", ""));
+}
+
+function compressAnswers() {
+  return LZString.compress(getAnswers().join('𓀴'));
+}
+
+function decompressAnswers(answers) {
+  return LZString.decompress(answers).split('𓀴');
+}
+
+
+var copyButton = $("<a>").addClass("navbtn link-btn sExtlink-processed").text("Copy Answers").on("click", e => GM_setClipboard(compressAnswers()))
+var pasteButton = $("<button>").text("Paste Answers").on("click", e => {
+  var answers = decompressAnswers(prompt("Enter your answer string"));
+  for(var i=0; i <  $(".form-text.blank.s-js-language-keyboard").length; i++) {
+    $(".form-text.blank.s-js-language-keyboard")[i].value = answers[i];
+  }
+})
+if($(".ui-selectmenu-status").length > 0) {
+  $(".navigator-buttons").prepend(copyButton);
+}
+
+
+if($(".progress").length > 0) {
+  $(".assessment-status.enabled").prepend(pasteButton);
+};
